f9e4273dad3a4059b1a8c481227369545f9bbc9a
Chuan Xu <xchuan@gmail.com>
2014-02-08 18:59:25 +0800

    fixed no margin top content bug

--- js/bottomFixed.js
+++ js/bottomFixed.js
@@ -8,11 +8,11 @@
 			var layer = this;
 		
 			//主要
-			var gaptop = (($('html').outerHeight(true)-$('body').height())<options.offsetMax && ($('html').outerHeight(true)-$('body').height())>options.offsetMin)?$('html').outerHeight(true)-$('body').height():options.offset;
-			if ($('html').data('gap')===undefined)$('html').data('gap',gaptop);
-			if(document.addEventListener){$('body').css('height',$('html').outerHeight(true)-gaptop);}
+			var gaptop = ((($('html').outerHeight(true)-$('body').height())<options.offsetMax && ($('html').outerHeight(true)-$('body').height())>options.offsetMin) || ($('html').outerHeight(true)-$('body').height())==0)?$('html').outerHeight(true)-$('body').height():options.offset;
+			if ($('html').data('gap')===undefined){$('html').data('gap',gaptop).css('height','100%');}
+			if(document.addEventListener){$('body').css('height',$('html').outerHeight(true)-$('html').data('gap'));}
 			var diff = $('html').height()-$(window).height();
-			if (document.all && document.querySelector && !document.addEventListener) {$('body').css({'height':$('html').outerHeight(true)-gaptop-diff,'min-height':$('html').outerHeight(true)-gaptop-diff});$('#ie8_redraw').html($('html').outerHeight(true));}
+			if (document.all && document.querySelector && !document.addEventListener) {$('body').css({'height':$('html').outerHeight(true)-$('html').data('gap')-diff,'min-height':$('html').outerHeight(true)-$('html').data('gap')-diff});$('#ie8_redraw').html($('html').outerHeight(true));}
 			$('.ftfix').css('margin-top',-$('.ftfix').outerHeight())
 		}
 		
