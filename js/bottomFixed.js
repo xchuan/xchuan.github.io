19cc36f6dc1ab2424eabaa69301982edaef9c662
Chuan Xu <xchuan@gmail.com>
2014-02-08 16:04:22 +0800

    add bottom fixed layout template

---
+++ js/bottomFixed.js
@@ -0,0 +1,34 @@
+﻿(function($){
+
+ 	$.fn.extend({ 
+ 		
+		bottomFixed: function(options) {
+
+			var options =  $.extend(defaults, options);
+			var layer = this;
+		
+			//主要
+			var gaptop = (($('html').outerHeight(true)-$('body').height())<options.offsetMax && ($('html').outerHeight(true)-$('body').height())>options.offsetMin)?$('html').outerHeight(true)-$('body').height():options.offset;
+			if ($('html').data('gap')===undefined)$('html').data('gap',gaptop);
+			if(document.addEventListener)$('body').css('height',$('html').outerHeight(true)-gaptop);
+			$('.ftfix').css('margin-top',-$('.ftfix').outerHeight())
+		}
+		
+	});
+
+	//默认设置
+	var defaults = {
+		offset:16,
+		offsetMax:20,
+		offsetMin:4,
+	}
+	
+})(jQuery);
+
+$(window).resize(function() {
+	$.fn.bottomFixed();
+});
+
+$(window).load(function() {
+	$.fn.bottomFixed();
+});
